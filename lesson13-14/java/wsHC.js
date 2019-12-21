5069105cd78c2233b83a8cdf3150f46e1e7f42f5
toadmaster93 <43352426+toadmaster93@users.noreply.github.com>
2019-12-21 00:37:19 -0700

    Add files via upload

--- lesson13-14/java/wsHC.js
+++ lesson13-14/java/wsHC.js
@@ -12,3 +12,21 @@ weatherObject1.onload = function() {
     document.getElementById("HumidityHC").innerHTML = weatherInfo.main.humidity;
 
 }
+function windChill() {
+    var tempF = parseFloat(document.getElementById("TemperatureHC").textContent);
+    var speed = parseFloat(document.getElementById("WindSpeedHC").textContent);
+    var chill = calculate(tempF, speed);
+
+    if (tempF <= 50 && speed > 3) {
+        document.getElementById('should').textContent = chill;
+    } else {
+        document.getElementById('should').textContent = "N/A";
+    }
+    
+    }
+
+function calculate(tempF, speed) {
+    var Temperature = 35.74 + (0.6215 * tempF) - (35.75 * (Math.pow(speed, 0.16))) + (0.4275  * tempF * (Math.pow(speed, 0.16)));
+    var wind = Temperature.toFixed(0);
+    return wind; 
+}

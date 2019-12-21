94a5e6f5a7f2dcb47c92863fb8b42c0f1bcf3319
toadmaster93 <43352426+toadmaster93@users.noreply.github.com>
2019-12-21 02:51:20 -0700

    Add files via upload

--- lesson13-14/java/wsWN.js
+++ lesson13-14/java/wsWN.js
@@ -12,3 +12,21 @@ weatherObject4.onload = function() {
     document.getElementById("HumidityWN").innerHTML = weatherInfo.main.humidity;
 
 }
+function windChill() {
+    var tempF = parseFloat(document.getElementById("TemperatureWN").textContent);
+    var speed = parseFloat(document.getElementById("WindSpeedWN").textContent);
+    var chill = calculate(tempF, speed);
+
+    if (tempF <= 50 && speed > 3) {
+        document.getElementById('shouldWN').textContent = chill;
+    } else {
+        document.getElementById('shouldWN').textContent = "N/A";
+    }
+    
+    }
+
+function calculate(tempF, speed) {
+    var Temperature = 35.74 + (0.6215 * tempF) - (35.75 * (Math.pow(speed, 0.16))) + (0.4275  * tempF * (Math.pow(speed, 0.16)));
+    var wind = Temperature.toFixed(0);
+    return wind; 
+}
